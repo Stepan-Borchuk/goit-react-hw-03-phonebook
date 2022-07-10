bb676efc257edc090d9800c68df57c91512cbb72
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-07-10 22:09:17 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Box } from './box/Box';
+import { Container, Title } from './App.styled';
+import { Component } from 'react';
+import { ContactForm } from './form/Form';
+import ContactList from './contacts/ContactsList';
+import { nanoid } from 'nanoid';
+import { Filter } from './filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    filter: '',
+  };
+
+  submitForm = (values, { resetForm }) => {
+    const oldContact = this.state.contacts.find(
+      person => person.name.toLowerCase() === values.name.toLowerCase()
+    );
+
+    if (oldContact) {
+      alert(` ${values.name} is already in contacts.`);
+      return;
+    }
+
+    const person = {
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    };
+
+    this.setState(prevState => {
+      return {
+        contacts: [person, ...prevState.contacts],
+      };
+    });
+    resetForm();
+  };
+
+  onFilter = evt => {
+    this.setState({
+      filter: evt.target.value,
+    });
+  };
+
+  onDelete = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(c => c.id !== id),
+      };
+    });
+  };
+
+  render() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const newContacts = this.state.contacts.filter(person =>
+      person.name.toLowerCase().includes(normalizedFilter)
+    );
+    return (
+      <Box
+        as={'div'}
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        color="white"
+        backgroundColor="#1d2327"
+      >
+        <Container>
+          <Title>Phonebook</Title>
+          <ContactForm submitForm={this.submitForm} />
+          <Filter onFilter={this.onFilter} filter={this.state.filter} />
+          <ContactList
+            contactsInfo={newContacts}
+            deleteContact={this.onDelete}
+          />
+        </Container>
+      </Box>
+    );
+  }
+}
